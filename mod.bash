e7485f458ac7bacbf1a0e7a84a709ad31339b4f7
Robert Gingras <developer@three-point-five.dev>
2024-04-06 17:41:01 -0400

    simple module loading system from the plugins directory

---
+++ mod.bash
@@ -0,0 +1,27 @@
+plugin_dir="${XDG_DATA_HOME:-"$HOME/.local/share"}/modbash/plugins"
+loaded_plugins=()
+
+load () {
+  plugin="$1"
+  source "$plugin_dir/$plugin.sh"
+  loaded_plugins+="$plugin"
+}
+
+require () {
+  plugin="$1"
+  if [[ ! "${loaded_plugins[@]}" =~ "$plugin" ]]; then
+    load "$plugin"
+  fi
+}
+
+optional_require () {
+  plugin="$1"
+  if [[ "${plugins[@]}" =~ "$plugin" ]]; then
+    require "$plugin"
+  fi
+}
+
+for plugin in "${plugins[@]}"; do
+  load "$plugin"
+done
+
